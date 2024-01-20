726482d7a26adeb5242a807095eba6c21bfa7c52
Mateo Vasco <macv9620@utp.edu.co>
2024-01-19 22:56:58 -0500

    Se agrega endpoint para probar disponibilidad y versión del servicio

---
+++ src/main/java/com/vertech/forest/web/controller/HealthCheckController.java
@@ -0,0 +1,31 @@
+package com.vertech.forest.web.controller;
+
+import com.vertech.forest.persistence.entity.QueryEntity;
+import com.vertech.forest.web.controller.wrapper.ResponseWrapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<ResponseWrapper<?>> getAll(){
+
+        String message = "Forest API - v.1.0.0";
+        List<QueryEntity> data = null;
+        HttpStatus httpStatus = HttpStatus.OK;
+
+        ResponseWrapper<List<QueryEntity>> responseWrapper = new ResponseWrapper<>(
+                message,
+                data
+        );
+
+        return new ResponseEntity<>(responseWrapper, httpStatus);
+    }
+}
